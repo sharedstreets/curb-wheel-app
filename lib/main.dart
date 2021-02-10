f17e6e2f49f72e99e91691aba688138816ccc25c
Kevin Webb <kpw@Kevins-MacBook-Pro.local>
2021-02-10 15:21:55 -0500

    fix map screen name change after merge

--- lib/main.dart
+++ lib/main.dart
@@ -39,54 +39,66 @@ class CurbWheel extends StatelessWidget {
                 color: Colors.black),
             subtitle2: TextStyle(fontSize: 14, fontStyle: FontStyle.italic),
             bodyText2: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
-          )
-        ),
-        onGenerateRoute: (settings) {
-          if (settings.name == WheelScreen.routeName) {
-            final WheelScreenArguments args = settings.arguments;
-            return MaterialPageRoute(
-              builder: (context) {
-                return WheelScreen(args.project, args.survey, args.incompleteSpans,
-                    listItem: args.listItem);
-              },
-            );
-          } if (settings.name == MapScreen.routeName) {
-            final MapScreenArguments args = settings.arguments;
-            return MaterialPageRoute(
-              builder: (context) {
-                return MapScreen(project: args.project);
-              },
-            );
-          } if (settings.name == CameraScreen.routeName) {
-            final CameraScreenArguments args = settings.arguments;
-            return MaterialPageRoute(
-              builder: (context) {
-                return CameraScreen(surveyItemId: args.surveyItemId, position: args.position, pointId: args.pointId,);
-              },
-            );
-          } if (settings.name == PreviewScreen.routeName) {
-            final PreviewScreenArguments args = settings.arguments;
-            return MaterialPageRoute(
-              builder: (context) {
-                return PreviewScreen(surveyItemId: args.surveyItemId, filePath: args.filePath, position: args.position, pointId: args.pointId);
-              },
-            );
-          } if (settings.name == StreetSelectMapScreen.routeName) {
-            final StreetSelectMapScreenArguments args = settings.arguments;
-            return MaterialPageRoute(
-              builder: (context) {
-                return StreetSelectMapScreen(project: args.project);
-              },
-            );
-          } else {
-            return null;
-          }
-        },
-        routes: {
-          SplashScreen.routeName: (context) => SplashScreen(),
-          ProjectListScreen.routeName: (context) => ProjectListScreen(),
-          FeatureSelectScreen.routeName: (context) => FeatureSelectScreen(),
-        },
-      );
+          )),
+      onGenerateRoute: (settings) {
+        if (settings.name == WheelScreen.routeName) {
+          final WheelScreenArguments args = settings.arguments;
+          return MaterialPageRoute(
+            builder: (context) {
+              return WheelScreen(
+                  args.project, args.survey, args.incompleteSpans,
+                  listItem: args.listItem);
+            },
+          );
+        }
+        if (settings.name == StreetSelectMapScreen.routeName) {
+          final StreetSelectMapScreenArguments args = settings.arguments;
+          return MaterialPageRoute(
+            builder: (context) {
+              return StreetSelectMapScreen(project: args.project);
+            },
+          );
+        }
+        if (settings.name == CameraScreen.routeName) {
+          final CameraScreenArguments args = settings.arguments;
+          return MaterialPageRoute(
+            builder: (context) {
+              return CameraScreen(
+                surveyItemId: args.surveyItemId,
+                position: args.position,
+                pointId: args.pointId,
+              );
+            },
+          );
+        }
+        if (settings.name == PreviewScreen.routeName) {
+          final PreviewScreenArguments args = settings.arguments;
+          return MaterialPageRoute(
+            builder: (context) {
+              return PreviewScreen(
+                  surveyItemId: args.surveyItemId,
+                  filePath: args.filePath,
+                  position: args.position,
+                  pointId: args.pointId);
+            },
+          );
+        }
+        if (settings.name == StreetSelectMapScreen.routeName) {
+          final StreetSelectMapScreenArguments args = settings.arguments;
+          return MaterialPageRoute(
+            builder: (context) {
+              return StreetSelectMapScreen(project: args.project);
+            },
+          );
+        } else {
+          return null;
+        }
+      },
+      routes: {
+        SplashScreen.routeName: (context) => SplashScreen(),
+        ProjectListScreen.routeName: (context) => ProjectListScreen(),
+        FeatureSelectScreen.routeName: (context) => FeatureSelectScreen(),
+      },
+    );
   }
 }
